e853c8e8b7276f68e1ebb8cedcc073fb140c8556
Ryan Scott <ryankennethscott@gmail.com>
2017-04-22 14:16:16 +1200

    Fixing body response

--- cricd.go
+++ cricd.go
@@ -602,10 +602,10 @@ func (d *Delivery) Push() (ok bool, err error) {
 		}
 		if res.StatusCode == http.StatusInternalServerError {
 			log.WithFields(log.Fields{"response": res.Status, "code": res.StatusCode, "body": body}).Errorf("Got not OK response from event API, now retrying attempt %d/5", i+1)
-			err = fmt.Errorf("Internal server error from event api - %s - %s", res.Status, body)
+			err = fmt.Errorf("Internal server error from event api - %s - %s", res.Status, string(body))
 		} else if res.StatusCode == http.StatusBadRequest {
 			log.WithFields(log.Fields{"response": res.Status, "code": res.StatusCode, "body": body}).Errorf("Bad request reported from event API")
-			err = fmt.Errorf("Bad request reported from event api - %s - %s", res.Status, body)
+			err = fmt.Errorf("Bad request reported from event api - %s - %s", res.Status, string(body))
 			return false, err
 
 		} else {
